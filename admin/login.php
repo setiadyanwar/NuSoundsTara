42c8735ed7a2f26be9319db5c07b0377b938fe90
SetiadyAnwar <setiadyanwar@gmail.com>
2024-05-13 13:42:58 +0700

    login

--- admin/login.php
+++ admin/login.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     require "../koneksi.php";
 
 ?>
@@ -30,7 +31,7 @@
   <body>
   <section class="vh-100" style="background-color: #f8cb2e;" >
   <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
+    <div class="row d-flex flex-column justify-content-center align-items-center h-100">
       <div class="col col-xl-10">
         <div class="card" style="border-radius: 1rem;">
           <div class="row g-0">
@@ -39,36 +40,59 @@
                 alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem; " />
             </div>
             <div class="col-md-6 col-lg-7 d-flex align-items-center">
-              <div class="card-body p-4 p-lg-5 text-black">
+              <div class="card-body p-4 p-lg-5 text-black login-box">
 
-                <form>
-
+                <form action="" method="post">
                   <div class="d-flex align-items-center mb-3 pb-1">
-                    
                     <span class="h1 fw-bold mb-0">NuSoundsTara</span>
                   </div>
 
                   <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
 
                   <div class="form-outline mb-4">
-                    <label class="form-label" for="form2Example17">Email address</label>
-                    <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                    <label class="form-label" for="username">Username</label>
+                    <input type="text" name="username" id="username" class="form-control form-control-lg" />
                   </div>
 
                   <div class="form-outline mb-4">
-                    <label class="form-label" for="form2Example27">Password</label>
-                    <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                    <label class="form-label" for="password">Password</label>
+                    <input type="password" name="password" id="password" class="form-control form-control-lg" />
                   </div>
 
-                  <div class="pt-1 mb-4">
-                    <button class="btn custom-btn btn-lg btn-block" type="button">Login</button>
+                  <div class="pt-1 mb-2">
+                    <button class="btn custom-btn btn-lg btn-block" type="submit" name="loginbtn">Login</button>
+                  </div>
+                  <div class="container">
+                    <div class="row">
+                      <div class="col">
+                        <?php
+                          if(isset($_POST['loginbtn'])){
+                            $username =htmlspecialchars( $_POST['username'] );
+                            $password =htmlspecialchars( $_POST['password'] );
+
+                            $query = mysqli_query($con, "SELECT * FROM users WHERE username ='$username'");
+
+                            $countdata = mysqli_num_rows($query);
+                            
+                            if($countdata >0){
+                              
+                            }else{
+                              ?>
+                              <div class="alert alert-warning" role="alert">
+                                Akun tidak tersedia
+                              </div>
+                              <?php
+                            }
+                          }
+                        ?>
+                      </div>
+                    </div>
                   </div>
 
                   <a class="small text-muted" href="#!">Forgot password?</a>
                   <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="#!"
                       style="color: #393f81;">Register here</a></p>
-                  <a href="#!" class="small text-muted">Terms of use.</a>
-                  <a href="#!" class="small text-muted">Privacy policy</a>
+                  
                 </form>
 
               </div>
@@ -78,6 +102,9 @@
       </div>
     </div>
   </div>
+
+ 
+
 </section>
         <!-- JAVASCRIPT FILES -->
         <script src="../js/jquery.min.js"></script>
